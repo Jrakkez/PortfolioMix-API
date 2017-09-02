6cfdfa5507b71ff8b374a183df2d37bc5a62ff80
Sam Martinez <sam.martinez.dev@gmail.com>
2017-09-02 17:37:55 -0500

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,13 +13,14 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mixer, Mixer.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "example.com", port: 80],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 config :mixer, Mixer.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE_URL"),
   pool_size: 20
 
 # Do not print debug messages in production
